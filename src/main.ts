44d91038170a525a8b7486c295f052b78f3b0cf2
mtjf development <dev@mtjfoundation.org>
2025-12-02 22:00:13 +0500

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // enable global validation
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  // enable CORS for local frontend development
-  app.enableCors();
 
-  const port = +(process.env.PORT || 3000);
+  app.enableCors({
+    origin: [
+      'http://localhost:5173', // local Vite
+      'http://localhost:3000',
+      'https://sargodha-event-fe.vercel.app', // ✅ frontend
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 3000;
   await app.listen(port);
+
   console.log(`Server running on http://localhost:${port}`);
 }
 bootstrap();
