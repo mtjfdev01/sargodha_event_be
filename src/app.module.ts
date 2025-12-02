6ee569b7cfdc700d54b7731fe446d834301f28d9
mtjf development <dev@mtjfoundation.org>
2025-12-02 17:54:32 +0500

    update 5:54

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,21 @@
 import { Module } from '@nestjs/common';
-import * as dotenv from 'dotenv';
-import { TypeOrmModule } from '@nestjs/typeorm';   // ✅ ADD THIS
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Donor } from './donor/entities/donor.entity';
 import { Donation } from './donation/entities/donation.entity';
 import { DonationsModule } from './donation/donation.module';
 import { DonorModule } from './donor/donor.module';
-dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: +(process.env.DB_PORT || 5432),
-      username: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASS || 'postgres',
-      database: process.env.DB_NAME || 'donations_db',
+      url: process.env.DATABASE_URL, // ✅ Railway Postgres
       entities: [Donor, Donation],
       synchronize: true,
       logging: false,
+      ssl: process.env.DATABASE_URL
+        ? { rejectUnauthorized: false }
+        : false,
     }),
     DonationsModule,
     DonorModule,
